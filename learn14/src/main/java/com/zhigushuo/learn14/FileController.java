e7f875d16aa5a194da54fd0728339fe2426fabdc
ls1072502993 <1072502993@qq.com>
2019-04-10 17:26:49 +0800

    springboot2，入门教程，-14-下载文件详解

---
+++ learn14/src/main/java/com/zhigushuo/learn14/FileController.java
@@ -0,0 +1,70 @@
+package com.zhigushuo.learn14;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.*;
+import java.util.List;
+
+@Controller
+@RequestMapping("file")
+public class FileController {
+    private static final Logger log = LoggerFactory.getLogger(FileController.class);
+
+    @GetMapping
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/download")
+    public String downloadFile(HttpServletRequest request, HttpServletResponse response) {
+        String fileName = "test.jpg";// 文件名
+        if (fileName != null) {
+            //设置文件路径
+            File file = new File("D:\\app\\learn13\\" + fileName);
+            //File file = new File(realPath , fileName);
+            if (file.exists()) {
+                response.setContentType("application/force-download");// 设置强制下载不打开
+                response.addHeader("Content-Disposition", "attachment;fileName=" + fileName);// 设置文件名
+                byte[] buffer = new byte[1024];
+                FileInputStream fis = null;
+                BufferedInputStream bis = null;
+                try {
+                    fis = new FileInputStream(file);
+                    bis = new BufferedInputStream(fis);
+                    OutputStream os = response.getOutputStream();
+                    int i = bis.read(buffer);
+                    while (i != -1) {
+                        os.write(buffer, 0, i);
+                        i = bis.read(buffer);
+                    }
+                    return "下载成功";
+                } catch (Exception e) {
+                    e.printStackTrace();
+                } finally {
+                    if (bis != null) {
+                        try {
+                            bis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    if (fis != null) {
+                        try {
+                            fis.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+        return "下载失败";
+    }
+}
---
+++ learn14/src/main/java/com/zhigushuo/learn14/Learn14Application.java
@@ -0,0 +1,13 @@
+package com.zhigushuo.learn14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn14Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn14Application.class, args);
+    }
+
+}
