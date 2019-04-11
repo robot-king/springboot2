d4cc03f5e74185d9053d4f5bce051bf07f9a6275
ls1072502993 <1072502993@qq.com>
2019-04-11 12:09:12 +0800

    springboot2，入门教程，-15-全局异常详解

---
+++ learn15/src/main/java/com/zhigushuo/learn15/Learn15Application.java
@@ -0,0 +1,13 @@
+package com.zhigushuo.learn15;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Learn15Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Learn15Application.class, args);
+    }
+
+}
